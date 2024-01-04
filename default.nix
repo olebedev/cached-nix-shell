73b2f5e99c5be2ac7286f06dec0a0b0e115e4ab1
Oleg Lebedev <oleg@canva.com>
2024-01-05 10:28:17 +1100

    clean up the default.nix a bit

--- default.nix
+++ default.nix
@@ -5,17 +5,30 @@ let
   naersk = pkgs.callPackage sources.naersk { };
   gitignoreSource = (pkgs.callPackage sources.gitignore { }).gitignoreSource;
   blake3-src = sources.BLAKE3;
+
+  # The main cached-nix-shell package. It's subject for override (see below) of the 
+  # underlying derivation attributes so the build works correctly.
+  package = naersk.buildPackage {
+    root = gitignoreSource ./.;
+    buildInputs = with pkgs; [
+      openssl
+      nix
+      ronn
+    ];
+  };
+  # Final overrides.
+  package' = package.overrideAttrs (_: {
+    CNS_GIT_COMMIT =
+      if builtins.pathExists ./.git
+      then pkgs.lib.commitIdFromGitRepo ./.git
+      else "next";
+    BLAKE3_CSRC = "${blake3-src}/c";
+    postBuild = ''
+      make -f nix/Makefile post-build
+    '';
+    postInstall = ''
+      make -f nix/Makefile post-install
+    '';
+  });
 in
-(naersk.buildPackage {
-  root = gitignoreSource ./.;
-  buildInputs = [ pkgs.openssl pkgs.nix pkgs.ronn ];
-}).overrideAttrs (attrs: {
-  CNS_GIT_COMMIT =
-    if builtins.pathExists ./.git then
-      pkgs.lib.commitIdFromGitRepo ./.git
-    else
-      "next";
-  BLAKE3_CSRC = "${blake3-src}/c";
-  postBuild = "make -f nix/Makefile post-build";
-  postInstall = "make -f nix/Makefile post-install";
-})
+package'
